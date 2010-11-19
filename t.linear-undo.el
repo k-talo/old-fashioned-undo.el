b931b079950fedf1d2fb34cd608e9fcc7ccea4c8
k-taro <Keitaro.Miyazaki@gmail.com>
2010-11-19 16:00:03 +0900

    Added copyrights lines to test file.

--- t.linear-undo.el
+++ t.linear-undo.el
@@ -1,4 +1,35 @@
+;;; t.linear-undo.el -- Tests for linear-undo.el
 
+;; Copyright (C) 2010 K-talo Miyazaki, all rights reserved.
+;; Author: K-talo Miyazaki <Keitaro dot Miyazaki at gmail dot com>
+;; Created: Fri Nov 19 14:13:19 2010 JST
+;; Keywords: emulation wp
+;; Revision: $Id$
+;; URL: 
+;; GitHub: 
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; TODO
+;;  - Write more tests.
+
+(eval-when-compile
+  (require 'el-test-more))
+
+(require 'linear-undo)
 
 ;; Test `linear-undo/lst/split'.
 ;;
